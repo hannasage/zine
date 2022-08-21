e9e4a6adc9a8a334a278d0138e64bf09140a410d
Kevin Haube <khaube@fearless.tech>
2022-08-20 22:23:44 -0400

    Quick refactor

--- src/components/ZinePage.tsx
+++ src/components/ZinePage.tsx
@@ -2,18 +2,10 @@ import { useEffect, useMemo } from "react";
 
 import ZinePageConfig from "../configs/ZinePageConfig";
 import { Template } from "../framework/Template";
-import { TEMPLATE_MAP } from "../templates";
 
 /** Controls the template generation and rendering of a page. */
 export const ZinePage = (config: ZinePageConfig) => {
-  const template = useMemo(
-    () =>
-      new Template({
-        props: config,
-        bundle: TEMPLATE_MAP.get(config.templateId)!!, // Throws inside Template if undefined
-      }),
-    [config]
-  );
+  const template = useMemo(() => new Template(config), [config]);
 
   useEffect(() => {
     template.validateProps(); // Throws inside if props are invalid
