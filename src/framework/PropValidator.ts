d92645d6b3b49fa95f1f3d32a7e94169a8a19066
Kevin Haube <khaube@fearless.tech>
2022-08-21 01:31:24 -0400

    Adds RuleGenerator type def

--- src/framework/PropValidator.ts
+++ src/framework/PropValidator.ts
@@ -2,6 +2,7 @@ import { ZinePageConfig } from "../configs";
 
 /** Function that validates props for a template */
 export type RuleFunction = (config: ZinePageConfig) => void;
+export type RuleGenerator = (...args: any[]) => RuleFunction;
 /** Contains all validation rules for a template */
 export default class PropValidator {
   rules: RuleFunction[] = [];
--- src/framework/common-rules.ts
+++ src/framework/common-rules.ts
@@ -1,13 +1,13 @@
 import { InvalidTemplatePropsError } from "../errors";
 import { ZinePageConfig } from "../configs";
 
-import { RuleFunction } from "./PropValidator";
+import { RuleFunction, RuleGenerator } from "./PropValidator";
 
 const imageLengthMessage = (actual: number, supported: number) =>
   `This template only supports ${supported} image, received: ${actual}`;
 /** RULE: Template only supports a specific amount of images
  * @throws {InvalidTemplatePropsError} */
-export const imageLengthCheck =
+export const imageLengthCheck: RuleGenerator =
   (length: number): RuleFunction =>
   (config: ZinePageConfig): void => {
     if (config.images.length > length)
@@ -20,7 +20,7 @@ const timeTooShortMessage = (actual: number, supported: number) =>
   `The viewTimeRequirement must exceed ${supported}. Received: ${actual}`;
 /** RULE: A minimum value (in ms) is required for viewTimeRequirement
  * @throws {InvalidTemplatePropsError} */
-export const viewTimeCheck =
+export const viewTimeCheck: RuleGenerator =
   (viewTime: number): RuleFunction =>
   (config: ZinePageConfig): void => {
     if (config.viewTimeRequirement < viewTime)
