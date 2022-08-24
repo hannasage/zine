06f846800d862e58f6a0851a9639cf24a0a983fb
Kevin Haube <khaube@fearless.tech>
2022-08-23 21:02:51 -0400

    Move hook to template renderer layer

--- src/templates/MainFrame/MainFrameTemplate.tsx
+++ src/templates/MainFrame/MainFrameTemplate.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-import { BasicTemplateProps, usePageTimer } from "../../framework";
+import { BasicTemplateProps } from "../../framework";
 import { Container, Frame, Image } from "../../components";
 
 /** TEMPLATE: A single image in a frame. */
 export const MainFrameTemplate: React.FC<BasicTemplateProps> = (props) => {
-  usePageTimer(props.viewTimeRequirement);
   return (
     <Container>
       <Frame width={87} height={87}>
