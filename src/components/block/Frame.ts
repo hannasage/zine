f05a98557fa1bd2d79f7d97f9e5a40352c7c2492
Kevin Haube <khaube@fearless.tech>
2022-08-24 21:51:14 -0400

    Zine CLI and first generator

--- src/components/block/Frame.ts
+++ src/components/block/Frame.ts
@@ -1,10 +1,4 @@
 import styled from "styled-components";
-
-export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-`;
 /** Width and height, in vw/vh percentage, for image frame.
  * The higher the percentage, the less border around the image. */
 interface FrameProps {
@@ -15,12 +9,4 @@ export const Frame = styled.div<FrameProps>`
   width: ${(props) => `${props?.width}%`};
   height: ${(props) => `${props?.height}%`};
   margin: auto;
-`;
-
-export const Image = styled.img`
-  display: block;
-  height: 100%;
-  width: auto;
-  margin: 0px auto;
-  object-fit: contain;
 `;
---
+++ zine.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [[ $1 == "-C" ]]
+then
+  /bin/bash ./cli/generators/make-component.sh
+fi
