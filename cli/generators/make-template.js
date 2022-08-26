268ccbc20d3e572da3ad78ab8236ccf3ca6f6530
Kevin Haube <khaube@fearless.tech>
2022-08-25 22:16:12 -0400

    Adds JSDOCs for most functions

--- cli/generators/make-template.js
+++ cli/generators/make-template.js
@@ -1,31 +1,70 @@
 const fs = require("fs");
-
+/** Create a dynamic anchor regex. Anchors are easier ways to access the end
+ * of object creators (i.e. `.set(...); //anchor`) so you can replace the anchor
+ * with the desired string
+ *
+ * @example
+ * ANCHOR_REGEX("TemplateMapAnchor") == //TemplateMapAnchor
+ * @param anchorTerm {string} The term in your anchor comment (no whitespace)
+ * @return RegExp
+ * */
 function ANCHOR_REGEX(anchorTerm) {
-  return RegExp(`;\\s\\/\\/${anchorTerm}`);
+  return RegExp(`;\\s\\/\\/${anchorTerm.trim()}`);
 }
-
-function templateMapCode(enumName, generatorName, rulesName) {
-  return `.set(
-    ${enumName},
-    { generator: ${generatorName}, rules: ${rulesName} }
-    );`;
-}
-
-function makeEnum(name) {
+/** Makes an `enum` key as a string.
+ *
+ * @example
+ * "my-component" -> "MY_COMPONENT"
+ * @param name {string} Name argument from CLI
+ * @returns string
+ * */
+function makeEnumName(name) {
   const enumKey = name.toUpperCase();
-  if (enumKey.includes("-")) {
-    return `${enumKey.replaceAll("-", "_")} = "${name}"`;
-  }
-  return `${enumKey} = "${name}"}`;
+  if (enumKey.includes("-")) return enumKey.replaceAll("-", "_");
+  return enumKey;
 }
-
-function makeRules(name) {
+/** Creates a `TemplateSetup` object as a string.
+ *
+ * @example
+ * "{ generator: MyComponentGenerator, rules: MyGeneratorRules }"
+ * @param name {string} Name argument from CLI
+ * @returns string
+ * */
+function makeTemplateSetup(name) {
   function template(generatorKey, rulesKey) {
     return `{ generator: ${generatorKey}, rules: ${rulesKey} }`;
   }
 }
-
-function makeGenerator(name) {
+/** Creates a string to replace the anchor at the end of the TEMPLATE map
+ * in `templates/index.ts`
+ *
+ * @param name {string} Name argument from CLI
+ * @return string
+ * */
+function makeSetupMapEntry(name) {
+  return `.set(
+    ${makeEnumName(name)},
+    ${makeTemplateSetup()}
+    ); //TemplateMapAnchor`;
+}
+/** Creates the full enum class entry as a string.
+ *
+ * @example
+ * 'NAME_PARAM = "name-param"'
+ * @param name {string} The name argument from CLI
+ * @return string
+ * */
+function makeEnumEntry(name) {
+  return `${makeEnumName(name)} = "${name}"}`;
+}
+/** Creates a template generator as a string.
+ *
+ * @example
+ * "export const MyTemplateGenerator = () => <MyTemplate />"
+ * @param name {string} The name argument from CLI
+ * @returns string
+ * */
+function makeGeneratorExport(name) {
   function template(templateComponentName) {
     return `export const ${templateComponentName}Generator = () => <${templateComponentName} />`;
   }
@@ -35,13 +74,13 @@ function makeConfigPiece(type, name) {
   if (!name || name === "") throw new Error("Name is a required variable");
   switch (type) {
     case "enum":
-      makeEnum(name);
+      makeEnumEntry(name);
       break;
     case "generator":
-      makeGenerator(name);
+      makeGeneratorExport(name);
       break;
-    case "rules":
-      makeRules(name);
+    case "setup":
+      makeSetupMapEntry(name);
       break;
     default:
       throw new Error(`Not a valid name type: ${type}`);
@@ -61,7 +100,7 @@ function makeTemplate(name) {
   );
   const modifiedIndexContents = indexContents.replace(
     ANCHOR_REGEX("anchor"),
-    templateMapCode()
+    makeConfigPiece()
   );
 
   fs.writeFileSync("./cli/_test/indexTest.ts", modifiedIndexContents);
