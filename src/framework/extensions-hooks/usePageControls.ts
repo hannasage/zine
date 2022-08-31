99caef46932ce70c4a96af268321ccd21cc132d7
Kevin Haube <khaube@fearless.tech>
2022-08-30 21:13:20 -0400

    Save Point: Keyboard controls buggy

---
+++ src/framework/extensions-hooks/usePageControls.ts
@@ -0,0 +1,35 @@
+/** FEATURE: Describe your feature
+ * */ import { useCallback, useEffect } from "react";
+
+import { usePageContext } from "../../components";
+
+export const usePageControls = (index: number) => {
+  const { scrollToRef } = usePageContext();
+  const scrollUp = useCallback(() => {
+    if (index === 0) return;
+    scrollToRef(index - 1);
+  }, [index, scrollToRef]);
+  const scrollDown = useCallback(() => {
+    scrollToRef(index + 1);
+  }, [index, scrollToRef]);
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.code === "ArrowUp") {
+        console.log("Logging");
+        return scrollUp();
+      }
+      if (e.code === "ArrowDown") {
+        return scrollDown();
+      }
+    },
+    [scrollDown, scrollUp]
+  );
+
+  return useEffect(() => {
+    window.addEventListener("keyup", handleKeyDown);
+    // cleanup this component
+    return () => {
+      window.removeEventListener("keyup", handleKeyDown);
+    };
+  }, [handleKeyDown]);
+};
